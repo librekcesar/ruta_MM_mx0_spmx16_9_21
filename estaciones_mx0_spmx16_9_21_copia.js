bfb202a6117897ba22d3bd49ee62a0914c17bb5a
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-16 16:05:03 -0600

    Add files via upload

---
+++ estaciones_mx0_spmx16_9_21_copia.js
@@ -0,0 +1,11 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_spmx16_9_21",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "MexiquenseParks, 54900, De las Torres 191, Sin Nombre, 54910 Fuentes del Valle, Méx.", "description": "MX0", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1476988, 19.6397536, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Av. Río de Guadalupe, Ciudad de México, CDMX", "description": "SPMX16", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.0942074, 19.472801, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Canal Ma. Candelaria 5, Barrio 18, Xochimilco, 16034 18, CDMX", "description": "SPMX9", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.1099551, 19.2826233, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Río Churubusco 76-38-38 38, Puente Blanco, Iztapalapa, 09770 Ciudad de México, CDMX", "description": "SPMX21", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.064434, 19.3290902, 0.0 ] } }
+]
+}
